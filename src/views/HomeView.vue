626fc3b8c93746358c3dd67ab59dca6d50fd5166
Luffy <lhu@li-jiang.com>
2025-07-21 15:35:30 +0800

    update home page width media of phone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,40 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { computed, ref } from 'vue'
 
-const age = ref<number>(31);
+const userInfo = ref({
+  name: '孟虎',
+  sex: '男',
+  phone: '13151634514',
+  email: 'meng1037127772@gmail.com',
+  education: '本科',
+  job: '前端工程师',
+  status: '在职',
+  location: '苏州',
+  blog: 'https://blog.mengyunyi.top',
+  github: 'https://github.com/menghu1994'
+})
+
+const age = computed(() => {
+  return new Date().getFullYear() - 1994
+})
+
+
+function copyTextToClipboard(text: string) {
+  try {
+    if (navigator?.clipboard?.writeText) {
+      navigator.clipboard.writeText(text);
+      alert('复制成功');
+    } else {
+      console.warn('Clipboard API not supported by this browser.');
+    }
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
+}
+
+const copy = () => {
+  copyTextToClipboard(userInfo.value.email)
+}
 </script>
 
 <template>
@@ -15,33 +48,40 @@ const age = ref<number>(31);
           <img src="../assets/images/head.jpg" class="header" />
           <div class="status">
             <div class="subtitle">
-              <p>本科</p>
-              <p>在职</p>
-              <p>苏州</p>
+              <p>{{ userInfo.education }}</p>
+              <p>{{ userInfo.location }}</p>
               <p>{{ age }}</p>
+              <p>{{ userInfo.status }}</p>
             </div>
           </div>
         </div>
-        <h2>孟虎</h2>
-        <p class="description">前端工程师,手机号<em>13151634514</em></p>
+        <h2>{{ userInfo.name }}</h2>
+        <p class="description">{{ userInfo.job }}<br />手机号<em>{{ userInfo.phone }}</em></p>
 
         <ul class="contactWay">
-          <li><a href="blog/"><img src="../assets/images/blog.png" alt="blog">
+          <li>
+            <a href="blog/"><img src="../assets/images/blog.png" alt="blog">
               <p>博客</p>
-            </a></li>
-          <li class="wechat"><img src="../assets/images/wechat_logo.png" alt="homepage">
+            </a>
+          </li>
+          <li class="wechat">
+            <img src="../assets/images/wechat_logo.png" alt="homepage">
             <p>微信</p>
             <img src="../assets/images/wechat.png" alt="" id="wechatQR">
           </li>
-          <li> <a href="https://github.com/menghu1994"><img src="../assets/images/github.png" alt="github">
+          <li>
+            <a href="https://github.com/menghu1994">
+              <img src="../assets/images/github.png" alt="github">
               <p>github</p>
-            </a></li>
-          <li><a href="#" onclick="copy()"><img src="../assets/images/gmail.png" title="meng1037127772@gmail.com" alt="gmail">
+            </a>
+          </li>
+          <li>
+            <a href="#" @click.prevent="copy()"><img src="../assets/images/gmail.png" :title="userInfo.email"
+                alt="gmail">
               <p>Gmail</p>
-              </a>
+            </a>
           </li>
         </ul>
-        <input type="text" id="gmailLink" value="meng1037127772@gmail.com" style="position: fixed;left:-2400px">
       </div>
       <div class="square" style="--i:0;"></div>
       <div class="square" style="--i:1;"></div>
@@ -59,85 +99,103 @@ const age = ref<number>(31);
   </section>
 </template>
 
-<style scoped>
-@import url("https://fonts.google.com/specimen/Poppins?selection.family=Open+Sans|Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900");
-
-body,
-section,
-ul,
-li,
-img,
-a,
-p,
-h2 {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
-}
-
-ul,
-li {
-  list-style: none;
-}
-
-a {
-  text-decoration: none;
-  color: #fff;
-}
-
+<style scoped lang="scss">
 section {
   display: flex;
   align-items: center;
   justify-content: center;
   min-height: 100vh;
   background: linear-gradient(to bottom, #f1f4f9, #dff1ff);
+
+  .color {
+    position: absolute;
+    filter: blur(150px);
+
+    &:nth-child(1) {
+      top: -350px;
+      right: -100px;
+      width: 600px;
+      height: 600px;
+      background: #F28DA8;
+    }
+
+    &:nth-child(2) {
+      bottom: -150px;
+      left: 100px;
+      width: 500px;
+      height: 500px;
+      background: #ace0f9;
+    }
+
+    &:nth-child(3) {
+      bottom: 50px;
+      left: 100px;
+      width: 300px;
+      height: 300px;
+      background: #D8D9D0;
+    }
+  }
 }
 
-section .color {
-  position: absolute;
-  filter: blur(150px);
-}
-
-section .color:nth-child(1) {
-  top: -350px;
-  right: -100px;
-  width: 600px;
-  height: 600px;
-  background: #F28DA8;
-}
-
-section .color:nth-child(2) {
-  bottom: -150px;
-  left: 100px;
-  width: 500px;
-  height: 500px;
-  background: #ace0f9;
-}
-
-section .color:nth-child(3) {
-  bottom: 50px;
-  left: 100px;
-  width: 300px;
-  height: 300px;
-  background: #D8D9D0;
-}
 
 .box {
   position: relative;
-}
 
-.box .square {
-  position: absolute;
-  backdrop-filter: blur(5px);
-  box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  border-right: 1px solid rgba(255, 255, 255, 0.2);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  background: rgba(255, 255, 255, 0.1);
-  border-radius: 10px;
-  animation: animate 10s linear infinite;
-  animation-delay: calc(-1s * var(--i));
+  .square {
+    position: absolute;
+    backdrop-filter: blur(5px);
+    box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    border-right: 1px solid rgba(255, 255, 255, 0.2);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 10px;
+    animation: animate 10s linear infinite;
+    animation-delay: calc(-1s * var(--i));
+
+    &:nth-child(1) {
+      top: -50px;
+      right: -60px;
+      width: 100px;
+      height: 100px;
+    }
+
+    &:nth-child(2) {
+      top: 10px;
+      left: -100px;
+      width: 120px;
+      height: 120px;
+      z-index: 2;
+    }
+
+    &:nth-child(3) {
+      top: 50px;
+      right: -60px;
+      width: 80px;
+      height: 80px;
+    }
+
+    &:nth-child(4) {
+      bottom: -80px;
+      left: 100px;
+      width: 50px;
+      height: 50px;
+    }
+
+    &:nth-child(5) {
+      bottom: -80px;
+      left: 140px;
+      width: 60px;
+      height: 60px;
+    }
+
+    &:nth-child(6) {
+      bottom: -180px;
+      left: 240px;
+      width: 60px;
+      height: 60px;
+    }
+  }
 }
 
 @keyframes animate {
@@ -150,42 +208,6 @@ section .color:nth-child(3) {
   50% {
     transform: translateY(40px);
   }
-}
-
-.box .square:nth-child(1) {
-  top: -50px;
-  right: -60px;
-  width: 100px;
-  height: 100px;
-}
-
-.box .square:nth-child(2) {
-  top: 10px;
-  left: -100px;
-  width: 120px;
-  height: 120px;
-  z-index: 2;
-}
-
-.box .square:nth-child(3) {
-  top: 50px;
-  right: -60px;
-  width: 80px;
-  height: 80px;
-}
-
-.box .square:nth-child(4) {
-  bottom: -80px;
-  left: 100px;
-  width: 50px;
-  height: 50px;
-}
-
-.box .square:nth-child(5) {
-  bottom: -80px;
-  left: 140px;
-  width: 60px;
-  height: 60px;
 }
 
 .container {
@@ -205,66 +227,75 @@ section .color:nth-child(3) {
   border-right: 1px solid rgba(255, 255, 255, 0.2);
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   padding: 30px;
+
+  h2 {
+    font-size: 1.5em;
+    font-weight: 600;
+    letter-spacing: 1px;
+    color: #333;
+    line-height: 1.5em;
+  }
+
+  p {
+    font-size: 1em;
+    font-weight: 600;
+    letter-spacing: 1px;
+  }
+
+  .description {
+    font-size: 1.3em;
+    margin: 20px;
+    text-align: center;
+    color: transparent;
+    background: -webkit-linear-gradient(to right, #fa2c05, #f637e3);
+    background-image: linear-gradient(90deg, #fa2c05 0%, #f637e3 50%, #8514f5 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .head {
+    position: relative;
+
+    .header {
+      width: 80px;
+      height: 80px;
+      border: 3px solid white;
+      border-radius: 50%;
+      padding: 2px;
+    }
+  }
+
+  .status {
+    position: absolute;
+    top: 18px;
+    right: -80px;
+    width: 70px;
+    height: 50px;
+    line-height: 50px;
+    box-shadow: 0 25px 45px rgb(0 0 0 / 10%);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    border-radius: 10px;
+    animation: pulse 2s linear infinite;
+    overflow: hidden;
+
+    .subtitle {
+      width: 100%;
+      position: absolute;
+      text-align: center;
+      animation: moveup 8s 1s infinite;
+
+      p {
+        background: linear-gradient(to right, red, blue);
+        color: transparent;
+        -webkit-background-clip: text;
+
+      }
+    }
+  }
+
 }
 
-.container h2 {
-  font-size: 1.5em;
-  font-weight: 600;
-  letter-spacing: 1px;
-  color: #333;
-  line-height: 1.5em;
-}
-
-.container p {
-  font-size: 1em;
-  font-weight: 600;
-  letter-spacing: 1px;
-}
-
-.container .description {
-  font-size: 1.3em;
-  margin: 20px;
-}
-
-.container .head {
-  position: relative;
-}
-
-.container .head .header {
-  width: 80px;
-  height: 80px;
-  border: 3px solid white;
-  border-radius: 50%;
-  padding: 2px;
-}
-
-.container .status {
-  position: absolute;
-  top: 18px;
-  right: -80px;
-  width: 70px;
-  height: 50px;
-  line-height: 50px;
-  box-shadow: 0 25px 45px rgb(0 0 0 / 10%);
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  border-radius: 10px;
-  animation: pulse 2s linear infinite;
-  overflow: hidden;
-}
-
-.container .status .subtitle {
-  width: 100%;
-  position: absolute;
-  text-align: center;
-  animation: moveup 8s 1s infinite;
-}
-
-.subtitle p {
-  background: linear-gradient(to right, red, blue);
-  color: transparent;
-  -webkit-background-clip: text;
-
-}
 
 @keyframes pulse {
   50% {
@@ -293,30 +324,34 @@ section .color:nth-child(3) {
 
 .contactWay {
   padding: 10px;
-  display: flex
-}
+  display: flex;
 
-.contactWay li {
-  text-align: center;
-}
+  li {
+    text-align: center;
 
-.contactWay li img {
-  width: 3.4em;
-  height: 3.4em;
-  background: rgba(255, 255, 255, 0.2);
-  padding: 6px;
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  border-right: 1px solid rgba(255, 255, 255, 0.2);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.05);
-  margin: 0 15px;
-  text-align: center;
-}
+    img {
+      width: 3.4em;
+      height: 3.4em;
+      background: rgba(255, 255, 255, 0.2);
+      padding: 6px;
+      border-radius: 10px;
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      border-right: 1px solid rgba(255, 255, 255, 0.2);
+      border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+      box-shadow: 0 5px 15px rgba(0, 0, 0, 0.05);
+      margin: 0 15px;
+      text-align: center;
+    }
 
+    p {
+      color: #525252;
+      font-size: 12px;
+    }
+  }
 
-.contactWay .wechat {
-  position: relative;
+  .wechat {
+    position: relative;
+  }
 }
 
 #wechatQR {
@@ -341,4 +376,72 @@ section .color:nth-child(3) {
   background: linear-gradient(to right, red, blue);
   -webkit-background-clip: text;
   color: transparent;
-}</style>
+}
+
+@media screen and (max-width: 767px) {
+  section {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container {
+    width: 90%;
+  }
+
+  .box .square {
+    animation: animate 7s linear infinite;
+
+    &:nth-child(1) {
+      top: -50px;
+      right: -60px;
+      width: 100px;
+      height: 100px;
+    }
+
+    &:nth-child(2) {
+      top: 15%;
+      left: 3%;
+      width: 100px;
+      height: 100px;
+      z-index: 2;
+    }
+
+    &:nth-child(3) {
+      top: 10%;
+      right: 0px;
+      width: 80px;
+      height: 80px;
+    }
+
+    &:nth-child(4) {
+      bottom: 22%;
+      left: 10%;
+      width: 50px;
+      height: 50px;
+    }
+
+    &:nth-child(5) {
+      bottom: 15%;
+      left: 30%;
+      width: 60px;
+      height: 60px;
+    }
+
+    &:nth-child(6) {
+      bottom: 130px;
+      right: 0;
+      width: 40px;
+      height: 40px;
+    }
+  }
+
+}
+</style>
